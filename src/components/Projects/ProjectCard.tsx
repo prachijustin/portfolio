7c598ecf90ec7ebccbdffc00086669a0d0d89d1e
prachijustin <prachi.sharma01sep@gmail.com>
2024-03-17 13:51:20 +0530

    project details

---
+++ src/components/Projects/ProjectCard.tsx
@@ -0,0 +1,23 @@
+import { getImageUrl } from "../../utils";
+import styles from "./ProjectCard.module.css";
+
+export const ProjectCard = ({
+  project: { title, imageSrc, description, skills },
+}) => {
+  return (
+    <div className={styles.container}>
+      <img src={getImageUrl(imageSrc)} alt={title} className={styles.image} />
+      <h3 className={styles.title}>{title}</h3>
+      <p className={styles.description}>{description}</p>
+      <ul className={styles.skills}>
+        {skills.map((skill, id) => {
+          return (
+            <li key={id} className={styles.skill}>
+              {skill}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
